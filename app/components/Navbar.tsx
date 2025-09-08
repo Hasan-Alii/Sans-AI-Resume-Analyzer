41aabfc2b35966648e6f9e6552d3b2ef96552a18
Hasan Ali <hasan.aly.dev@gmail.com>
2025-09-08 23:50:31 +0300

    Update Navbar and wipe route logic
    Navbar now conditionally shows 'Delete Resumes' link based on file presence and uses a button for sign out. Unused variables were removed from the wipe route for cleaner code.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,42 @@
-import { Link } from "react-router"
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
-
-
 const Navbar = () => {
-    const { auth } = usePuterStore();
-    return (
-      <nav className="navbar">
-        <Link to="/">
-          <p className="text-2xl font-bold text-gradient">San's Resume</p>
-        </Link>
-        {auth.isAuthenticated && (
-          <Link to="/auth" className="primary-button w-fit">
-            Sign Out
-          </Link>
-        )}
+  const { auth, fs } = usePuterStore();
+  const [files, setFiles] = useState<FSItem[]>();
 
-        <Link to="/upload" className="primary-button w-fit">
-          Upload Resume
-        </Link>
-      </nav>
-    );
-}
+  useEffect(() => {
+    const loadFiles = async () => {
+      const files = (await fs.readDir("./")) as FSItem[];
+      setFiles(files);
+    };
+    loadFiles();
+  }, [files]);
 
-export default Navbar
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">San's Resume</p>
+      </Link>
+
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+
+      {auth.isAuthenticated && (
+        <button className="primary-button w-fit" onClick={auth.signOut}>
+          Sign Out
+        </button>
+      )}
+
+      {files && (
+        <Link to="/wipe" className="primary-button w-fit">
+          Delete Resumes
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
